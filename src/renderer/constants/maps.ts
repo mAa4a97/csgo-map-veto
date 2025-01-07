a86d6660ddbbceab09383e69fc038fee7515cb1f
mAa4a <zobbroslv@gmail.com>
2025-01-07 15:55:29 +0200

    Updated map veto

--- src/renderer/constants/maps.ts
+++ src/renderer/constants/maps.ts
@@ -1,11 +1,10 @@
 export const MAPS = [
   'de_dust2',
   'de_inferno',
-  'de_cache',
-  'de_cobblestone',
   'de_mirage',
-  'de_overpass',
   'de_nuke',
   'de_train',
   'de_vertigo',
+  'de_ancient',
+  'de_anubis'
 ];
